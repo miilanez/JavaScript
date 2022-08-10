1b0c2986d11245ed07c7c309a963cd6cd98ad4e6
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2022-08-09 21:53:06 -0300

    att

---
+++ #0 - Básico/index.js
@@ -0,0 +1,30 @@
+//palavras reservadas : let, function, if, else, while, for, const, etc.
+
+//declaração de variável com valor
+let a;
+a = 3;
+console.log(a);
+
+//operadores aritméticos : +, -, *, /, %(resto), **(exponenciação)
+
+//operadores de atribuição : = , 
+
+let num = 3;
+num *= 2;
+console.log(num); // 6
+
+// op. atribuição *= faz com que seja a mesma coisa que "num = num * 2;"
+
+
+//operadores relacionais : >
+
+let az = 10;
+let bz = 20;
+console.log(az > 10); //false
+
+//operadores de igualdade : == , ===
+
+let nota1 = 5;
+let nota2 = '5';
+console.log(nota1 == nota2); // true, pois não considera o tipo
+console.log(nota1 === nota2); // false, pois considera o tipo
