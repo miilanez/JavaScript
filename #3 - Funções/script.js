a6f24d635947a86b9cb75569f40269df04eda774
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2022-08-09 23:14:27 -0300

    att modulo

---
+++ #3 - Funções/script.js
@@ -0,0 +1,9 @@
+//função - function
+
+//declaração da função
+function exibirMensagem(primeiroNome, ultimoNome) { //parâmetros/variáveis
+    alert (`Olá ${primeiroNome} ${ultimoNome}, seja bem vindo(a)!`);
+}
+
+//chamada ou invocação da função
+exibirMensagem('Gabriel', "Milanez"); //parâmetros inseridos
---
+++ #4 - Conversão de Distância/script.js
@@ -0,0 +1,6 @@
+function calculaDistancia(){
+    let km = document.querySelector('#valorKm').value;
+    document.querySelector('#resultado').textContent = km * 1.609;
+}
+
+console.log(km);
