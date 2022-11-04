cc25eef2ee414aa7efe96d524f223c416b15f4ae
Gabriel Milanez <gabrielmilanez99@hotmail.com>
2022-11-04 11:35:21 -0300

    aula de operadores de compração e lógicos

---
+++ #11 - Operadores/script.js
@@ -0,0 +1,29 @@
+//Operadores de Comparação
+
+/* 
+> Maior que
+>= Maior ou igual
+< Menor que
+<= Menor ou igual
+== igual a *********** não usar
+=== igual em valor e em tipo (igualdade estrita)
+!= diferente *********** não usar
+!== diferente estrito ( diferente em valor ou tipo)
+
+*/
+
+//Operadores Lógicos
+
+/*
+&& = and = e
+|| = or  = ou
+!  = not = não 
+*/
+
+//Mini Atividade
+
+const usuario = 'Gabriel';
+const senha = '123456';
+
+const vaiLogar = usuario === 'Gabriel' && senha === '123456';
+console.log(vaiLogar)
